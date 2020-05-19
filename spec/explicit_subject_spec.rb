2a0738b32fff4df8327fac166b288f5426ff25ad
Ife Ologbese <ologbesei@yahoo.com>
2020-05-19 18:28:09 +0200

    rspec subjects (implicit and explicit)

---
+++ spec/explicit_subject_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Hash do
+  subject(:bob) do
+    { a: 1, b: 2 }
+  end
+
+  it 'has two key-value pairs' do
+    expect(subject.length).to eq(2)
+    expect(bob.length).to eq(2)
+  end
+
+  it 'nested example' do
+    expect(subject.length).to eq(2)
+    expect(bob.length).to eq(2)
+  end
+end
